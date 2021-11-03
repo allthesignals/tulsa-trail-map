b345425fec745988a2ada67e71860dac06c3d9d1
allthesignals <>
2021-11-02 22:58:07 -0500

    Add basic styling

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'national-flag-blue': '#162B49',
+        mustard: '#EBAD21',
+        bone: '#F8F1E0',
+        'bright-red': '#C6202E',
+      },
+    },
   },
   variants: {
     extend: {},
