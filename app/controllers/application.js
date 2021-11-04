4cf1f62fd225c17b1f5914c9043edb1a1f7bc814
allthesignals <>
2021-11-03 21:40:11 -0500

    Add favicon

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -39,6 +39,7 @@ export default class ApplicationController extends Controller {
   proposed = true;
 
   trailStyle = {
+    id: 'trails',
     type:'line',
     paint: {
       ...EXISTING_TRAILS_PAINT,
@@ -52,4 +53,32 @@ export default class ApplicationController extends Controller {
       'line-width': 2,
     }
   };
+
+  onMapLoad(map) {
+    // this.setupHoverEffects(map);
+  }
+
+  // setupHoverEffects(map) {
+  //   map.on('mousemove', 'state-fills', (e) => {
+  //     if (e.features.length > 0) {
+  //       if (hoveredStateId !== null) {
+  //         map.setFeatureState({ source: 'states', id: hoveredStateId }, { hover: false });
+  //       }
+
+  //       hoveredStateId = e.features[0].id;
+
+  //       map.setFeatureState({ source: 'states', id: hoveredStateId }, { hover: true });
+  //     }
+  //   });
+
+  //   map.on('mouseleave', 'state-fills', () => {
+  //     if (hoveredStateId !== null) {
+  //     map.setFeatureState(
+  //     { source: 'states', id: hoveredStateId },
+  //     { hover: false }
+  //     );
+  //     }
+  //     hoveredStateId = null;
+  //   });
+  // }
 }
