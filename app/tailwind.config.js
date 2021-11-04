80bdeb8e0ee5e58042463979d24bb98bd390f524
allthesignals <>
2021-11-03 20:11:35 -0500

    Design tweaks

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
         bone: '#F8F1E0',
         'bright-red': '#C6202E',
       },
+      borderWidth: {
+        '10': '10px',
+        '12': '12px',
+      },
+      margin: {
+        '22': '5.5rem',
+      },
     },
   },
   variants: {
